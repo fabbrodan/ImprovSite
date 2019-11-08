358ed6869ff3a9edc6526a8389f74086cce71141
Daniel Åslund <daniel.aaslund@gmail.com>
2019-11-08 15:31:33 +0100

    Added theme switcher functionality and additional css files

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ $("document").ready(function() {
         sessionStorage.setItem("timerStart", new Date);
     }
     siteTimer();
+
+    if (sessionStorage.getItem("theme") == null) {
+        sessionStorage.setItem("theme", "dark");
+    }
+    else {
+        if (sessionStorage.getItem("theme") == "dark") {
+            $('link').attr("href", "darktheme.css");
+        }
+        else if (sessionStorage.getItem("theme") == "light") {
+            $('link').attr("href", "lighttheme.css");
+        }
+    }
+
+    $("#themeswitcher").click(function() {
+        if (sessionStorage.getItem("theme") == "dark") {
+            sessionStorage.setItem("theme", "light");
+            $('link').attr("href", "lighttheme.css");
+            $("#themeswitcher").attr("value", "Dark Theme");
+        }
+        else if (sessionStorage.getItem("theme") == "light") {
+            sessionStorage.setItem("theme", "dark");
+            $('link').attr("href", "darktheme.css");
+            $("themeswitcher").attr("value", "Light Theme");
+        }
+    });
 });
 
 // function to start a timer based off the start Date parameter
