528caab90f35d553a7c219396e534d5a97439729
Lime Fortytwo <melberik@gmail.com>
2019-11-12 15:00:30 +0100

    Added LastUpdate() - method  in javascript and formated the date of last update to something more readable.

--- Scripts/script.js
+++ Scripts/script.js
@@ -23,6 +23,8 @@ $("document").ready(function() {
     $("#themeswitcher").click(function() {
         switchTheme();
     });
+
+    LastUpdated();
 });
 
 // function to start a timer based off the start Date parameter
@@ -45,4 +47,15 @@ function switchTheme() {
         $("#themeswitcher").attr("value", "Light Theme");
         $("#headerImg").attr("src", "Images/Improviseramera_dark.png");
     }
+}
+
+// Get data about when the document was last modified as a date. Then show it
+// as a DateString. Then find the element with the correct class and have that
+// show the string. 
+function LastUpdated() {
+    var date = new Date(document.lastModified).toDateString();
+    var el = document.getElementsByClassName("date");
+
+    // Atm I don't know how to make javascript show "Å" so I did it like this. 
+    el[0].textContent = "Daniel \u00C5slund & Emil Rundberg" + " " + "(" + date + ")";
 }
