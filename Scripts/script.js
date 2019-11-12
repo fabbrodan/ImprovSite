59e660ec8a675c1cab987b1f66ed39ef2d142cc2
Daniel Åslund <daniel.aaslund@gmail.com>
2019-11-12 17:13:28 +0100

    Added check box functionality on project page

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,11 @@
 $("document").ready(function() {
+    // Start the timer on first time the site is loaded during the session
     if (sessionStorage.getItem("timerStart") == null) {
         sessionStorage.setItem("timerStart", new Date);
     }
     siteTimer();
 
+    // set the theme according to session storage
     if (sessionStorage.getItem("theme") == null) {
         sessionStorage.setItem("theme", "dark");
         $("#themeswitcher").attr("value", "Light Theme");
@@ -21,11 +23,25 @@ $("document").ready(function() {
         }
     }
 
+    // bind the theme switching funtion to the themeswitcher button
     $("#themeswitcher").click(function() {
         switchTheme();
     });
 
     LastUpdated();
+
+    // bind the checkbox function to checkboxes click event
+    $(".projectChkBox").on("click", function() {
+        setChecked($(this));
+    });
+
+    // set checked attribute on checkboxes depending on local storage
+    var checkBoxes = document.getElementsByClassName("projectChkBox")
+    Array.from(checkBoxes).forEach((element) => {
+        if (localStorage.getItem(element.getAttribute("id")) != null) {
+            element.setAttribute("checked", "true");
+        }
+    });
 });
 
 // function to start a timer based off the start Date parameter
@@ -35,6 +51,7 @@ function siteTimer() {
         }, 1000);
 }
 
+// Switch the theme and update button values on click of the button
 function switchTheme() {
     if (sessionStorage.getItem("theme") == "dark") {
         sessionStorage.setItem("theme", "light");
@@ -59,4 +76,14 @@ function LastUpdated() {
 
     // Atm I don't know how to make javascript show "�" so I did it like this. 
     el[0].textContent = "Daniel \u00C5slund & Emil Rundberg" + " " + "(" + date + ")";
+}
+
+// set and remove the local storage values for the checked boxes to be remembered across sessions
+function setChecked(chkBox) {
+    if (localStorage.getItem(chkBox.attr("id")) == null) {
+        localStorage.setItem(chkBox.attr("id"), "true");
+    }
+    else {
+        localStorage.removeItem(chkBox.attr("id"));
+    }
 }
